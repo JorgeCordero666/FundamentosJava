eb775c88eea77d9ab3ac128127fb1bfe7f4df811
JorgeCordero666 <jorge666.cordero@gmail.com>
2022-07-27 14:22:40 -0500

    Actualizacion del curso 27/07/222

--- src/Teoria/CicloWhile.java
+++ src/Teoria/CicloWhile.java
@@ -5,12 +5,19 @@ public class CicloWhile {
 
     public static void main(String[] args) {
         int contador =0;
+        int numero = 0;
         //Definicion Ciclo While
+        System.out.println("***Ciclo While***");
         while(contador <= 3){
             System.out.println("El contador esta en: " + contador);
             contador ++;
-
+ 
         }
+        //Definicion ciclo do-while
+        System.out.println("***Ciclo Do-While***");
+        do{
+            System.out.println("El contador esta en: " + numero);
+            numero ++;  
+        }while(numero < 3 );
     }
-    
 }
