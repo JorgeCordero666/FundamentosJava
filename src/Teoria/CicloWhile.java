7a79d368e4603e24926d747ddef9018f0ba848e1
JorgeCordero666 <jorge666.cordero@gmail.com>
2022-07-27 11:04:18 -0500

    Actualizacion 27/07/2022

---
+++ src/Teoria/CicloWhile.java
@@ -0,0 +1,16 @@
+
+package Teoria;
+
+public class CicloWhile {
+
+    public static void main(String[] args) {
+        int contador =0;
+        //Definicion Ciclo While
+        while(contador <= 3){
+            System.out.println("El contador esta en: " + contador);
+            contador ++;
+
+        }
+    }
+    
+}
